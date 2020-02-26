7cf797a489205e2fdea1284249289a6b60f5e911
malvinmarcelino <malvin.marcelino@gmail.com>
2020-02-26 12:24:24 +0700

    change flask key

--- ijazah_validation/__init__.py
+++ ijazah_validation/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = 'b578ac34d1c6c2ca4b7f374741ff6c1c'
 
 from ijazah_validation import routes
